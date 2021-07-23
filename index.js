1f4a167034b67d2cc5a825011c0571a9ef13e29b
luisbartolome <luisbartolomeoyarzabal@hotmail.es>
2021-07-23 11:41:46 +0200

    Edited models path and some status codes

--- index.js
+++ index.js
@@ -95,41 +95,153 @@ app.get('/movies/genres/:genres', (req, res) => {
 app.get('/movies/directors/:name', (req, res) => {
     res.send('Successful GET request returning a description of the Director');
 });
-
+// Get all users
+app.get('/users', (req, res) => {
+    Users.find()
+        .then((users) => {
+            res.status(200).json(users);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
+// Add Users
 app.post('/users', (req, res) => {
-    res.send('Registration succesful!');
+    // check if a user with the username provided by the client already exists
+    Users.findOne({ Username: req.body.Username })
+        .then((user) => {
+            if (user) {
+                return res.status(409).send(req.body.Username + 'already exists');
+            } else {
+                Users.create({
+                        Username: req.body.Username,
+                        Password: req.body.Password,
+                        Email: req.body.Email,
+                        Birthday: req.body.Birthday,
+                    })
+                    .then((user) => {
+                        //this callback, send a response back to the client that contains both a status code and the document (called “user”) just created
+                        res.status(201).json(user);
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                        res.status(500).send('Error: ' + error);
+                    });
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+        });
 });
 
-app.put('/users/:username', (req, res) => {
-    res.send({
-        movie: req.params.username,
-        updated: true,
-    })
-})
+// Get a user by username
+app.get('/users/:Username', (req, res) => {
+    Users.findOne({ Username: req.params.Username })
+        .then((user) => {
+            res.json(user);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
+// Update a user's info, by username
+/* We’ll expect JSON in this format
+{
+  Username: String,
+  (required)
+  Password: String,
+  (required)
+  Email: String,
+  (required)
+  Birthday: Date
+}*/
 
+app.put('/users/:username', (req, res) => {
+    Users.findOneAndUpdate(
+        //First parameter
+        { Username: req.params.Username },
+        //Second parameter
+        {
+            //SET new values that extracted from the request body (meaning that they come from a request sent by the user).
+            $set: {
+                Username: req.body.Username,
+                Password: req.body.Password,
+                Email: req.body.Email,
+                Birthday: req.body.Birthday,
+            },
+        },
+        //Third parameter
+        { new: true }, // This line makes sure that the updated document is returned
+        //Fourth parameter that itself have two parameters (any errors that might have occured + represents the document that was just updated )
+        (err, updatedUser) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send('Error: ' + err);
+            } else {
+                res.json(updatedUser);
+            }
+        }
+    );
+});
+
+// Add a movie to a user's list of favorites
+app.post('/users/:Username/favorites/:MovieID', (req, res) => {
+    Users.findOneAndUpdate({ Username: req.params.Username }, {
+            $push: { FavoriteMovies: req.params.MovieID }
+        }, { new: true }, // This line makes sure that the updated document is returned
+        (err, updatedUser) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send('Error: ' + err);
+            } else {
+                res.json(updatedUser);
+            }
+        });
+});
 
 app.patch('/users/:username/favourites/:title', (req, res) => {
     res.send({
         movie: req.params.title,
         favourite: true,
     })
-})
-
-app.delete('/users/:username/favorites/:title', (req, res) => {
-    res.send({
-        movie: req.params.title,
-        deleted: true,
-    })
-})
-
-app.delete('/users/:username', (req, res) => {
-    res.send("Successful DELETE request for user unregister");
 });
-
-// Error Handling
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Ups! Something is not working!');
+// “Allow users to remove a movie from their list of favorites” endpoint
+app.delete('/users/:Username/favorites/:MovieID', (req, res) => {
+    Users.findOneAndUpdate({ Username: req.params.Username }, {
+            $pull: { FavoriteMovies: req.params.MovieID },
+        }, { new: true }, // This line makes sure that the updated document is returned
+        (err, updatedUser) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send('Error: ' + err);
+            } else {
+                res.json(updatedUser);
+            }
+        }
+    );
+});
+// Delete a user by username
+app.delete('/users/:Username', (req, res) => {
+    Users.findOneAndRemove({ Username: req.params.Username })
+        .then((user) => {
+            if (!user) {
+                res.status(400).send(req.params.Username + ' was not found');
+            } else {
+                res.status(200).send(req.params.Username + ' was deleted.');
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+    // Error Handling
+    app.use((err, req, res, next) => {
+        console.error(err.stack);
+        res.status(500).send('Ups! Something is not working!');
+    });
 });
 
 // determine the port to listen on by checking PORT first and giving it a value
