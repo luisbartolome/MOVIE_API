8da4865ed571f75587fe895934729dccf947bf7c
luisbartolome <luisbartolomeoyarzabal@hotmail.es>
2021-08-13 19:56:16 +0200

    modify code

--- index.js
+++ index.js
@@ -18,10 +18,7 @@ const Models = require('./models/models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect(process.env.CONNECTION_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 //Serving static files middleware
