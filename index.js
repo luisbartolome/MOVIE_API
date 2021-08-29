72c0678f6c2bad078703dd1a64c88a07ffa83a00
luisbartolome <luisbartolomeoyarzabal@hotmail.es>
2021-08-29 20:10:28 +0200

    allowed Origins to cors

--- index.js
+++ index.js
@@ -7,7 +7,30 @@ const morgan = require('morgan');
 const app = express();
 //Cors access (allowed domains)
 const cors = require('cors');
-app.use(cors());
+let allowedOrigins = [
+    "http://localhost:8080",
+    "http://testsite.com",
+    "http://localhost:1234",
+    "https://luisbartolome.github.io/myFlix-client/",
+    "https://backend-myflix1.herokuapp.com/movies",
+    "http://localhost:4200/",
+];
+
+app.use(
+    cors({
+        origin: (origin, callback) => {
+            if (!origin) return callback(null, true);
+            if (allowedOrigins.indexOf(origin) === -1) {
+                // If a specific origin isn’t found on the list of allowed origins
+                let message =
+                    "The CORS policy for this application doesn’t allow access from origin " +
+                    origin;
+                return callback(new Error(message), false);
+            }
+            return callback(null, true);
+        },
+    })
+);
 
 const { check, validationResult } = require('express-validator');
 
