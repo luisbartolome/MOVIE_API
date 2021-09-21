6fd716b2f6350d6b48657761b6986db387a4a523
luisbartolome <luisbartolomeoyarzabal@hotmail.es>
2021-09-21 12:57:25 +0200

    update index.js

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.get("/", (req, res) => {
 
 //return JSON object when at /movies
 
-app.get("/movies", function(req, res) {
+app.get("/movies", passport.authenticate('jwt', { session: false }), (req, res) => {
     Movies.find()
         .then(function(movies) {
             res.status(201).json(movies);
@@ -82,7 +82,9 @@ app.get("/movies", function(req, res) {
 
 
 // GETS JSON movie info when looking for specific title
-app.get('/movies/:Title', function(req, res) {
+app.get('/movies/:Title', passport.authenticate('jwt', {
+    session: false
+}), (req, res) => {
     Movies.findOne({ Title: req.params.Title })
         .then((movie) => {
             res.status(200).json(movie);
@@ -95,53 +97,57 @@ app.get('/movies/:Title', function(req, res) {
 
 
 //Return data about a genre (description) by name/title (e.g., "Drama")
-app.get('/movies/genres/:Genre', function(req, res) {
-    Movies.findOne({ 'Genre.Name': req.params.Genre })
-        .then((genre) => {
-            res.status(200).json(genre.Genre);
-        })
-        .catch((err) => {
-            console.error(err);
-            res.status(500).send('Error: ' + err);
-        });
-});
+app.get('/movies/genres/:Genre', passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        Movies.findOne({ 'Genre.Name': req.params.Genre })
+            .then((genre) => {
+                res.status(200).json(genre.Genre);
+            })
+            .catch((err) => {
+                console.error(err);
+                res.status(500).send('Error: ' + err);
+            });
+    });
 
 // get all directors
 
-app.get('/directors', function(req, res) {
-    Directors.find()
-        .then((Director) => {
-            res.status(200).json(Director);
-        })
-        .catch((err) => {
-            console.error(err);
-            res.status(500).send('Error: ' + err);
-        });
-});
+app.get('/directors', passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        Directors.find()
+            .then((Director) => {
+                res.status(200).json(Director);
+            })
+            .catch((err) => {
+                console.error(err);
+                res.status(500).send('Error: ' + err);
+            });
+    });
 
 //get info on director when looking for specific director
-app.get('/movies/director/:Name', function(req, res) {
-    Movies.findOne({ 'Director.Name': req.params.Name })
-        .then((director) => {
-            res.status(200).json(director.Director);
-        })
-        .catch((err) => {
-            console.error(err);
-            res.status(500).send('Error: ' + err);
-        });
-});
+app.get('/movies/director/:Name', passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        Movies.findOne({ 'Director.Name': req.params.Name })
+            .then((director) => {
+                res.status(200).json(director.Director);
+            })
+            .catch((err) => {
+                console.error(err);
+                res.status(500).send('Error: ' + err);
+            });
+    });
 
 // Get all users
-app.get('/users', function(req, res) {
-    Users.find()
-        .then((users) => {
-            res.status(200).json(users);
-        })
-        .catch((err) => {
-            console.error(err);
-            res.status(500).send('Error: ' + err);
-        });
-});
+app.get('/users', passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        Users.find()
+            .then((users) => {
+                res.status(200).json(users);
+            })
+            .catch((err) => {
+                console.error(err);
+                res.status(500).send('Error: ' + err);
+            });
+    });
 // Add Users
 app.post(
     '/users', [
@@ -225,87 +231,91 @@ app.get('/users/:Username', [
   Birthday: Date
 }*/
 
-app.put('/users/:username', function(req, res) {
-    Users.findOneAndUpdate(
-        //First parameter
-        { Username: req.params.Username },
-        //Second parameter
-        {
-            //SET new values that extracted from the request body (meaning that they come from a request sent by the user).
-            $set: {
-                Username: req.body.Username,
-                Password: req.body.Password,
-                Email: req.body.Email,
-                Birthday: req.body.Birthday,
+app.put('/users/:username', passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        Users.findOneAndUpdate(
+            //First parameter
+            { Username: req.params.Username },
+            //Second parameter
+            {
+                //SET new values that extracted from the request body (meaning that they come from a request sent by the user).
+                $set: {
+                    Username: req.body.Username,
+                    Password: req.body.Password,
+                    Email: req.body.Email,
+                    Birthday: req.body.Birthday,
+                },
             },
-        },
-        //Third parameter
-        { new: true }, // This line makes sure that the updated document is returned
-        //Fourth parameter that itself have two parameters (any errors that might have occured + represents the document that was just updated )
-        (err, updatedUser) => {
-            if (err) {
-                console.error(err);
-                res.status(500).send('Error: ' + err);
-            } else {
-                res.json(updatedUser);
+            //Third parameter
+            { new: true }, // This line makes sure that the updated document is returned
+            //Fourth parameter that itself have two parameters (any errors that might have occured + represents the document that was just updated )
+            (err, updatedUser) => {
+                if (err) {
+                    console.error(err);
+                    res.status(500).send('Error: ' + err);
+                } else {
+                    res.json(updatedUser);
+                }
             }
-        }
-    );
-});
+        );
+    });
 
 // Add a movie to a user's list of favorites
-app.patch('/users/:Username/favorites/:MovieID', function(req, res) {
-    Users.findOneAndUpdate({ Username: req.params.Username }, {
-            $push: { FavoriteMovies: req.params.MovieID }
-        }, { new: true }, // This line makes sure that the updated document is returned
-        (err, updatedUser) => {
-            if (err) {
-                console.error(err);
-                res.status(500).send('Error: ' + err);
-            } else {
-                res.json(updatedUser);
-            }
-        });
-});
+app.patch('/users/:Username/favorites/:MovieID', passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        Users.findOneAndUpdate({ Username: req.params.Username }, {
+                $push: { FavoriteMovies: req.params.MovieID }
+            }, { new: true }, // This line makes sure that the updated document is returned
+            (err, updatedUser) => {
+                if (err) {
+                    console.error(err);
+                    res.status(500).send('Error: ' + err);
+                } else {
+                    res.json(updatedUser);
+                }
+            });
+    });
 
 // “Allow users to remove a movie from their list of favorites” endpoint
-app.delete('/users/:Username/favorites/:MovieID', function(req, res) {
-    Users.findOneAndUpdate({ Username: req.params.Username }, {
-            $pull: { FavoriteMovies: req.params.MovieID },
-        }, { new: true }, // This line makes sure that the updated document is returned
-        (err, updatedUser) => {
-            if (err) {
+app.delete('/users/:Username/favorites/:MovieID', passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        Users.findOneAndUpdate({ Username: req.params.Username }, {
+                $pull: { FavoriteMovies: req.params.MovieID },
+            }, { new: true }, // This line makes sure that the updated document is returned
+            (err, updatedUser) => {
+                if (err) {
+                    console.error(err);
+                    res.status(500).send('Error: ' + err);
+                } else {
+                    res.json(updatedUser);
+                }
+            }
+        );
+    });
+// Delete a user by username
+app.delete('/users/:Username', passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        Users.findOneAndRemove({ Username: req.params.Username })
+            .then((user) => {
+                if (!user) {
+                    res.status(400).send(req.params.Username + ' was not found');
+                } else {
+                    res.status(200).send(req.params.Username + ' was deleted.');
+                }
+            })
+            .catch((err) => {
                 console.error(err);
                 res.status(500).send('Error: ' + err);
-            } else {
-                res.json(updatedUser);
-            }
-        }
-    );
-});
-// Delete a user by username
-app.delete('/users/:Username', function(req, res) {
-    Users.findOneAndRemove({ Username: req.params.Username })
-        .then((user) => {
-            if (!user) {
-                res.status(400).send(req.params.Username + ' was not found');
-            } else {
-                res.status(200).send(req.params.Username + ' was deleted.');
-            }
-        })
-        .catch((err) => {
-            console.error(err);
-            res.status(500).send('Error: ' + err);
+            });
+
+        app.use(express.static('public'));
+
+        // Error Handling
+        app.use((err, req, res, next) => {
+            console.error(err.stack);
+            res.status(500).send('Ups! Something is not working!');
         });
-
-    app.use(express.static('public'));
-
-    // Error Handling
-    app.use((err, req, res, next) => {
-        console.error(err.stack);
-        res.status(500).send('Ups! Something is not working!');
     });
-});
 
 // determine the port to listen on by checking PORT first and giving it a value
 const port = process.env.PORT || 8080;
